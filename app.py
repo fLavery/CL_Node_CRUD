6420895b47bc00a2b620b7fde1fd734a54ab5bb4
Ubuntu <ubuntu@ip-172-31-18-56.us-east-2.compute.internal>
2019-08-06 10:40:18 +0000

    DB connected

--- app.py
+++ app.py
@@ -1,17 +1,29 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
-app= Flask(__name__)
+app = Flask(__name__)
+
+app.config['MONGO_DBNAME'] = os.environ.get('MONGO_DBNAME')
+app.config['MONGO_URI'] = os.environ.get('MONGO_URI')
+
+mongo = PyMongo(app)
 
 @app.route('/')
-def index():
-    user = os.environ.get('USER')
-    password = os.environ.get('PASSWORD')
-    return render_template('index.html', user=user, password=password)
+@app.route('/get_tasks')
+def get_tasks():
+    return render_template('tasks.html', tasks=mongo.db.tasks.find())
+
+# @app.route('/')
+# def index():
+#     user = os.environ.get('USER')
+#     password = os.environ.get('PASSWORD')
+#     return render_template('index.html', user=user, password=password)
  
-@app.route('/about')
-def about():
-    return render_template('about.html')    
+# @app.route('/about')
+# def about():
+#     return render_template('about.html')    
 
 
 if __name__ == '__main__':
