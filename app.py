db34fc25767c582f2d8fa8794cd7945d466632b6
Ubuntu <ubuntu@ip-172-31-18-56.us-east-2.compute.internal>
2019-08-07 14:14:06 +0000

    add task added

--- app.py
+++ app.py
@@ -13,24 +13,23 @@ mongo = PyMongo(app)
 @app.route('/')
 @app.route('/get_tasks')
 def get_tasks():
-    return render_template('tasks.html', tasks=mongo.db.tasks.find())
+    return render_template("tasks.html", 
+                           tasks=mongo.db.tasks.find())
+
 
 @app.route('/add_task')
 def add_task():
-    return render_template('addtask.html')
+    return render_template('addtask.html',
+                           categories=mongo.db.categories.find())
 
 
-# @app.route('/')
-# def index():
-#     user = os.environ.get('USER')
-#     password = os.environ.get('PASSWORD')
-#     return render_template('index.html', user=user, password=password)
- 
-# @app.route('/about')
-# def about():
-#     return render_template('about.html')    
-
-
+@app.route('/insert_task', methods=['POST'])
+def insert_task():
+    tasks = mongo.db.tasks
+    tasks.insert_one(request.form.to_dict())
+    return redirect(url_for('get_tasks'))
+    
+    
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
